9ce7f26c433051afb461313c71ea28669cf9e95d
Alexandre <alexandresluzbittencourt@gmail.com>
2022-04-25 13:59:38 -0300

    Criação de variaveis no csharp

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            int idadeJoao;
+
+            idade = 32;
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idadeJoao = 10;
+            Console.WriteLine(idadeJoao);
+
+            idade = 10 + 15;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Sua idade é: " + idade);
+
+            Console.WriteLine("Execução finalizada... tecle enter para sair");
+            Console.ReadLine();
+
+        }
+    }
+}
